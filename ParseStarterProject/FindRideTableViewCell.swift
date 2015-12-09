874b20c5f0d3dc47eaf5b761f2e207e98c939146
xyju <xyju90@gmail.com>
2015-12-08 17:34:50 -0800

    Fixed join and leave.

--- ParseStarterProject/FindRideTableViewCell.swift
+++ ParseStarterProject/FindRideTableViewCell.swift
@@ -15,9 +15,10 @@ class FindRideTableViewCell: PFTableViewCell {
     @IBOutlet weak var from: UILabel!
     @IBOutlet weak var date: UILabel!
     @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var seats: UILabel!
     var rideid: String?
-
+    var joined:Bool = false
     @IBAction func join(sender: UIButton) {
         
         let view = self.superview?.superview
@@ -25,7 +26,10 @@ class FindRideTableViewCell: PFTableViewCell {
         let tableView = view as! UITableView
         
         let query = PFQuery(className:"rides")
-        query.getObjectInBackgroundWithId(rideid!) {
+
+        if !joined{
+            
+                query.getObjectInBackgroundWithId(rideid!) {
             (ride: PFObject?, error: NSError?) -> Void in
             if error != nil {
                 print(error)
@@ -63,11 +67,60 @@ class FindRideTableViewCell: PFTableViewCell {
                 }
             }
             tableView.reloadData()
+            }
+            
+            joined = !joined
+            button.setTitle("Leave", forState: .Normal)
+        } else{
+            
+            query.getObjectInBackgroundWithId(rideid!) {
+                (ride: PFObject?, error: NSError?) -> Void in
+                if error != nil {
+                    print(error)
+                } else if let ride = ride {
+                    if ride["count"]! as! NSNumber == 0 {
+                        print("Ride is empty")
+                        
+                    }
+                    else {
+                        ride.incrementKey("count", byAmount: -1)
+                        ride.saveInBackgroundWithBlock {
+                            (success: Bool, error: NSError?) -> Void in
+                            if (success) {
+                                
+                                
+                                let currentuser = PFUser.currentUser()
+                                currentuser?.removeObject(self.rideid!, forKey: "ridelist")
+                                
+                                
+                                currentuser!.saveInBackgroundWithBlock {
+                                    (success: Bool, error: NSError?) -> Void in
+                                    if (success) {
+                                        print("success")
+                                        
+                                    } else {
+                                        print(error?.description)
+                                    }
+                                }
+                                
+                                
+                                
+                            } else {
+                                print(error)
+                            }
+                        }
+                    }
+                }
+                tableView.reloadData()
+            }
+            
+            joined = !joined
+            button.setTitle("Join", forState: .Normal)
         }
         
         
         
     }
-
+    
 
 }
